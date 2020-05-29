66665fbf51da03ff2f9318bebda4caf705e522f4
Gourdji19 <58864028+Gourdji19@users.noreply.github.com>
2020-05-29 23:02:55 +0300

    Add files via upload

---
+++ TeorVer/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace TeorVer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Номер = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Величина = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.LightBlue;
+            this.tabPage5.Controls.Add(this.button2);
+            this.tabPage5.Controls.Add(this.textBox6);
+            this.tabPage5.Controls.Add(this.textBox8);
+            this.tabPage5.Controls.Add(this.textBox7);
+            this.tabPage5.Controls.Add(this.label6);
+            this.tabPage5.Controls.Add(this.dataGridView5);
+            this.tabPage5.Controls.Add(this.dataGridView4);
+            this.tabPage5.Controls.Add(this.label7);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage5.Size = new System.Drawing.Size(1191, 424);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Проверка гипотезы";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 5);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 31);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Рассчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(313, 11);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(68, 20);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(558, 176);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(123, 20);
+            this.textBox8.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(558, 152);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(123, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(176, 14);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Уровень значимости  α :";
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column13,
+            this.Column11,
+            this.Column12});
+            this.dataGridView5.Location = new System.Drawing.Point(179, 40);
+            this.dataGridView5.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowTemplate.Height = 28;
+            this.dataGridView5.Size = new System.Drawing.Size(343, 361);
+            this.dataGridView5.TabIndex = 6;
+            this.dataGridView5.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView5_CellContentClick);
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Номер промежутка";
+            this.Column13.Name = "Column13";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Теоретическая вероятность попадания в промежуток";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Число попаданий в промежуток";
+            this.Column12.Name = "Column12";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2});
+            this.dataGridView4.Location = new System.Drawing.Point(16, 40);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowTemplate.Height = 28;
+            this.dataGridView4.Size = new System.Drawing.Size(142, 361);
+            this.dataGridView4.TabIndex = 5;
+            this.dataGridView4.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView4_CellContentClick);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Границы";
+            this.Column2.Name = "Column2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(593, 128);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = " F (R0) : ";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.LightBlue;
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(1191, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Исходные данные";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 33);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 5);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Испытание";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Номер,
+            this.Величина});
+            this.dataGridView1.Location = new System.Drawing.Point(324, 5);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(283, 408);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Номер
+            // 
+            this.Номер.HeaderText = "Номер";
+            this.Номер.Name = "Номер";
+            // 
+            // Величина
+            // 
+            this.Величина.HeaderText = "Величина";
+            this.Величина.Name = "Величина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Итераций";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Параметр σ ";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1199, 450);
+            this.tabControl1.TabIndex = 3;
+            this.tabControl1.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 430);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Номер;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Величина;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+
+    }
+}
+
---
+++ TeorVer/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ZedGraph;
+using System;
+
+namespace TeorVer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        void ShellSort(double[] mass)
+        {
+            int i, j, step;
+            double tmp1;
+            int n = mass.Length;
+            for (step = n / 2; step > 0; step /= 2)
+            {
+                for (i = step; i < n; i++)
+                {
+                    tmp1 = mass[i];
+                    for (j = i; j >= step; j -= step)
+                    {
+                        if (tmp1 < mass[j - step])
+                        {
+                            mass[j] = mass[j - step];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    mass[j] = tmp1;
+                }
+            }
+        }
+
+
+        private double Gamma(double Num)
+        {
+            if (Num > 1)
+            {
+                double Number = (Num - 1) * Gamma(Num - 1);
+                return Number;
+            }
+            if (Num == 1.0)
+            {
+                return 1.0;
+            }
+
+            return Math.Pow(3.14159, 0.5);
+        }
+
+        private double Hi_Quadrat(double R0, double k)
+        {
+            double gamma = Gamma((k - 1.0) / 2.0);
+            double result = 0;
+            double exp2 = 1;
+            for (int i = 0; i < k - 1; i++)
+            {
+                exp2 *= 2.0;
+            }
+            exp2 = Math.Pow(exp2, 0.5);
+            if (k == 2)
+            {
+                for (int i = 2; i <= 1000; i++)
+                {
+                    result += (Math.Exp(-R0 * ((double)i - 1.0) / 1000) * Math.Pow(R0 * ((double)i - 1.0) / 1000, (k - 1.0) / 2 - 1) / exp2 / gamma +
+                               Math.Exp(-R0 * (double)i / 1000) * Math.Pow(R0 * (double)i / 1000, (k - 1.0) / 2 - 1) / exp2 / gamma) * R0 / 2000;
+                }
+                return result;
+            }
+            for (int i = 1; i <= 1000; i++)
+            {
+                result += (Math.Exp(-R0 * ((double)i - 1.0) / 1000) * Math.Pow(R0 * ((double)i - 1.0) / 1000, (k - 1.0) / 2 - 1) / exp2 / gamma +
+                           Math.Exp(-R0 * (double)i / 1000) * Math.Pow(R0 * (double)i / 1000, (k - 1.0) / 2 - 1) / exp2 / gamma) * R0 / 2000;
+            }
+            return result;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //1 лаба
+            dataGridView1.Rows.Clear();
+
+            Random rnd = new Random(DateTime.Now.Second);
+
+            double sigma = Convert.ToDouble(textBox1.Text);
+            int Number_steps = Convert.ToInt32(textBox2.Text);
+            double[] y = new double[Number_steps];
+
+            for (int i = 0; i < Number_steps; i++)
+            {
+
+                y[i] = sigma * Math.Sqrt(-2 * Math.Log(rnd.NextDouble()));
+            }
+
+            ShellSort(y);
+            for (int i = 0; i < y.Length; i++)
+            {
+                dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[i].Cells[0].Value = i;
+                dataGridView1.Rows[i].Cells[1].Value = y[i];
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView5.Rows.Clear();
+
+
+            int Number_steps = Convert.ToInt32(textBox2.Text);
+            double sigma = Convert.ToDouble(textBox1.Text);
+            double[] y = new double[Number_steps];
+
+            double[] NumberExpInDistance = new Double[dataGridView4.RowCount];
+            for (int i = 0; i < Number_steps; i++)
+            {
+                y[i] = (double)dataGridView1.Rows[i].Cells[1].Value;
+            }
+
+            double[] q = new Double[dataGridView4.RowCount];
+
+            //Вычисление R0
+            double R0 = 0;
+            double sum = 0;
+
+
+            q[0] = 1 - Math.Exp(-Convert.ToDouble(dataGridView4[0, 0].Value) * Convert.ToDouble(dataGridView4[0, 0].Value) / (2 * sigma * sigma));
+            sum += q[0];
+            for (int i = 1; i < dataGridView4.RowCount - 1; i++)
+            {
+
+                q[i] = -Math.Exp(-Convert.ToDouble(dataGridView4[0, i].Value) * Convert.ToDouble(dataGridView4[0, i].Value) / (2 * sigma * sigma))
+                      + Math.Exp(-Convert.ToDouble(dataGridView4[0, i - 1].Value) * Convert.ToDouble(dataGridView4[0, i - 1].Value) / (2 * sigma * sigma));
+                sum += q[i];
+            }
+
+            q[q.Length - 1] = 1 - sum;
+            NumberExpInDistance[0] = 0;
+            double alpha = Convert.ToDouble(textBox6.Text);
+            int Numflag = 0;
+            for (int j = Numflag; j < y.Length; j++)
+            {
+                if (y[j] <= Convert.ToDouble(dataGridView4[0, 0].Value))
+                {
+                    NumberExpInDistance[0] += 1;
+                }
+                else
+                {
+                    Numflag = j;
+                    break;
+                }
+            }
+            for (int i = 1; i < dataGridView4.RowCount - 1; i++)
+            {
+                for (int j = Numflag; j < y.Length; j++)
+                {
+                    if (y[j] <= Convert.ToDouble(dataGridView4[0, i].Value) && y[j] > Convert.ToDouble(dataGridView4[0, i - 1].Value))
+                    {
+                        NumberExpInDistance[i] += 1;
+                    }
+                    else
+                    {
+                        Numflag = j;
+                        break;
+                    }
+                }
+            }
+            for (int j = Numflag; j < y.Length; j++)
+            {
+                if (y[j] > Convert.ToDouble(dataGridView4[0, dataGridView4.RowCount - 1].Value))
+                {
+                    NumberExpInDistance[NumberExpInDistance.Length - 1] += 1;
+                }
+            }
+            for (int i = 0; i < q.Length; i++)
+            {
+                if (q[i] != 0)
+                {
+                    R0 += Math.Pow(NumberExpInDistance[i] - (double)Number_steps * q[i], 2) / ((double)Number_steps * q[i]);
+                }
+            }
+            //Вывод
+            double FR = 1 - Hi_Quadrat(R0, q.Length);
+            textBox7.Text = Convert.ToString(FR);
+            for (int i = 0; i < q.Length; i++)
+            {
+                dataGridView5.Rows.Add(i, q[i], NumberExpInDistance[i]);
+            }
+            if (FR > alpha)
+            {
+                textBox8.Text = "Гипотеза принята";
+            }
+            else
+            {
+                textBox8.Text = "Гипотеза отвергнута";
+            }
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView5_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView4_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
